ff67a7c2630b72586ea7e2347cd0c3d6568a96fb
tungphien <tungphien.cn08@gmail.com>
2019-05-18 16:44:01 +0700

    add code for read all keywords from agni folder

---
+++ read_agni.py
@@ -0,0 +1,40 @@
+import glob
+import re
+
+agni_directory_path = "D:\\Startup\\agni\\"
+
+list_file_paths = glob.glob(agni_directory_path + "*.yaml")
+
+yaml_contents = ''
+for file_path in list_file_paths:
+    with open(file_path, 'r') as fr:
+        yaml_content = fr.read()
+        yaml_contents = yaml_contents + '\n' + yaml_content
+# print(yaml_contents)
+
+list_keywords = re.findall('keyword:(.*)\n', yaml_contents)
+keywords_without_run_event = {}
+for keyword in list_keywords:
+
+    if not keyword.strip().lower() == 'on config' and not keyword.strip().lower() == 'on cli':
+        keyword_name = ''
+        keyword=keyword.strip()
+        keyword_content = re.sub(r"\'", "", keyword)
+        keyword_splited_arr = re.split(r'\s{4,}', keyword_content)
+        if len(keyword_splited_arr) == 1:
+            keyword_name = keyword_splited_arr[0].strip()
+        else:
+            str = re.sub(r"\$|\'|\"", "", keyword_content)
+            for item in re.split(r'\s{3,}', str):
+                if not '{' in item and not '=' in item:
+                    keyword_name = item.strip()
+                    # print keyword_name
+                    break
+        if not keyword_name == '':
+            if not keywords_without_run_event:
+                if keyword_name not in keywords_without_run_event.keys():
+                    keywords_without_run_event[keyword_name] = keyword_content
+            else:
+                keywords_without_run_event[keyword_name] = keyword_content
+
+print keywords_without_run_event
